86b29329f32ce386a0d97635ecb56b63e29f0447
Arthur <arthur.perigno@gmail.com>
2025-02-05 17:54:00 +0100

    Polish main menu

--- ProjetMobile/Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ ProjetMobile/Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -7,6 +7,9 @@ public class MainMenuManager : MonoBehaviour
     [Header("References")] 
     [SerializeField] private RectTransform sagaMapParentRectTr;
     [SerializeField] private RectTransform upgradesParentRectTr;
+    [SerializeField] private RectTransform sagaMapIconRectTr;
+    [SerializeField] private RectTransform upgradeIconRectTr;
+    [SerializeField] private RectTransform selectedPanelIcon;
 
 
     private void Start()
@@ -18,11 +21,17 @@ public class MainMenuManager : MonoBehaviour
     {
         sagaMapParentRectTr.UChangeLocalPosition(0.5f, new Vector3(0, 0, 0), CurveType.EaseOutSin);
         upgradesParentRectTr.UChangeLocalPosition(0.5f, new Vector3(500, 0, 0), CurveType.EaseOutSin);
+        
+        selectedPanelIcon.UChangePosition(0.5f, new Vector3(sagaMapIconRectTr.position.x, selectedPanelIcon.position.y, 
+            selectedPanelIcon.position.z), CurveType.EaseOutCubic);
     }
 
     public void OpenUpgrades()
     {
         sagaMapParentRectTr.UChangeLocalPosition(0.5f, new Vector3(-500, 0, 0), CurveType.EaseOutSin);
         upgradesParentRectTr.UChangeLocalPosition(0.5f, new Vector3(0, 0, 0), CurveType.EaseOutSin);
+        
+        selectedPanelIcon.UChangePosition(0.5f, new Vector3(upgradeIconRectTr.position.x, selectedPanelIcon.position.y,
+            selectedPanelIcon.position.z), CurveType.EaseOutCubic);
     }
 }
